55f6a683603e65bc6da3442812c253db65f455fc
Gustavo <gugafantato@gmail.com>
2026-08-14 15:07:56 -0300

    Spring addition

--- src/main/java/com/gustavofantato/blackjack/strategy/ConservativeStrategy.java
+++ src/main/java/com/gustavofantato/blackjack/strategy/ConservativeStrategy.java
@@ -2,12 +2,14 @@ package com.gustavofantato.blackjack.strategy;
 
 import com.gustavofantato.blackjack.model.Deck;
 import com.gustavofantato.blackjack.model.Hand;
+import org.springframework.stereotype.Component;
 
 /** MODE [2]: CONSERVATIVE
  * AI MODE
  * The AI passes [P] if the current score is 15>=
  **/
 
+@Component
 public class ConservativeStrategy implements PlayerStrategy{
 
     @Override
